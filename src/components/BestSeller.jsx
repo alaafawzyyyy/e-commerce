944a446179a70c4aff8625524a2b3e17255d809a
alaa shaaban <alaa.shaaban943@gmail.com>
2025-11-11 16:15:49 +0200

    best seller section

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -1,30 +1,31 @@
 import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context";
 import { ProductItem, Title } from "../components";
-function LatestCollection() {
+function BestSeller() {
   const { products } = useContext(ShopContext);
-  const [latestProducts, setLatestProducts] = useState([]);
+  const [bestSeller, setBestSeller] = useState([]);
   useEffect(() => {
-    setLatestProducts(products.slice(20, 30));
+    const bestProduct = products.filter((item) => item.bestseller);
+    setBestSeller(bestProduct.slice(0, 5));
   }, []);
 
   return (
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
-        <Title text1="LATEST" text2="COLLECTION" />
+        <Title text1="BEST" text2="SELLERS" />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the.
         </p>
       </div>
-      {/* {rendering products} */}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {latestProducts.map((item, index) => (
-          <ProductItem key={index} item={item} />
+        {/* {rendering products} */}
+        {bestSeller.map((item, index) => (
+          <ProductItem item={item} key={index} />
         ))}
       </div>
     </div>
   );
 }
 
-export default LatestCollection;
+export default BestSeller;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { Hero } from "../components";
 import { LatestCollection } from "../components";
+import BestSeller from "../components/BestSeller";
 function Home() {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
     </div>
   );
 }
